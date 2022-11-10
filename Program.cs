f77ef15bc6933a18c79a4c5b72fcc5463201ba8f
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-11-10 22:10:12 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystemDatabase
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Address Book System program");
+        }
+    }
+}
